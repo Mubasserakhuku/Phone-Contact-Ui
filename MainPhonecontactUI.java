52c6138955bc70b99909304f20d7f6bb7019dadf
MubasseraKhuku <mubasserabaust@53gmail.com>
2026-06-01 11:06:59 -0700

    Initial Commit - Netbeans Project

---
+++ MainPhonecontactUI.java
@@ -0,0 +1,113 @@
+
+package main.phonecontactui;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class MainPhonecontactUI extends Application {
+    // TableView
+    private TableView<Contact> table;
+
+    // Data List
+    private ObservableList<Contact> contactList =
+            FXCollections.observableArrayList();
+
+    @Override
+    public void start(Stage stage) {
+
+        // ===== TITLE =====
+        Label title = new Label("Phone Contact Manager");
+        title.setStyle("-fx-font-size: 22px; -fx-font-weight: bold;");
+
+        // ===== INPUT FIELDS =====
+        TextField nameField = new TextField();
+        nameField.setPromptText("Enter Name");
+
+        TextField phoneField = new TextField();
+        phoneField.setPromptText("Enter Phone Number");
+
+        TextField emailField = new TextField();
+        emailField.setPromptText("Enter Email");
+
+        // ===== BUTTONS =====
+        Button addBtn = new Button("Add");
+        Button updateBtn = new Button("Update");
+        Button deleteBtn = new Button("Delete");
+        Button clearBtn = new Button("Clear");
+
+        addBtn.setPrefWidth(100);
+        updateBtn.setPrefWidth(100);
+        deleteBtn.setPrefWidth(100);
+        clearBtn.setPrefWidth(100);
+
+        // ===== TABLE VIEW =====
+        table = new TableView<>();
+
+        TableColumn<Contact, String> nameCol =
+                new TableColumn<>("Name");
+        nameCol.setMinWidth(180);
+        nameCol.setCellValueFactory(
+                new PropertyValueFactory<>("name"));
+
+        TableColumn<Contact, String> phoneCol =
+                new TableColumn<>("Phone");
+        phoneCol.setMinWidth(180);
+        phoneCol.setCellValueFactory(
+                new PropertyValueFactory<>("phone"));
+
+        TableColumn<Contact, String> emailCol =
+                new TableColumn<>("Email");
+        emailCol.setMinWidth(220);
+        emailCol.setCellValueFactory(
+                new PropertyValueFactory<>("email"));
+
+        table.setItems(contactList);
+
+        table.getColumns().addAll(nameCol, phoneCol, emailCol);
+
+        // ===== ADD BUTTON ACTION =====
+        addBtn.setOnAction(e -> {
+
+            String name = nameField.getText();
+            String phone = phoneField.getText();
+            String email = emailField.getText();
+
+            if(name.isEmpty() || phone.isEmpty() || email.isEmpty()){
+
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all fields!");
+                alert.showAndWait();
+
+            } else {
+
+                contactList.add(
+                        new Contact(name, phone, email)
+                );
+
+                nameField.clear();
+                phoneField.clear();
+                emailField.clear();
+            }
+        });
+
+
+    
+  
+
---
+++ Software-Devlopment-lab-1-/README.md
@@ -0,0 +1,36 @@
+#software_development_project
+---
+
+## Theory:
+
+
+### Git
+Bangladesh is known as the "Land of Rivers," hosts the world's largest mangrove forest (the Sundarbans), home to the *Royal Bengal Tiger*, has the world's longest natural sea beach (Cox's Bazar), and experiences **six** distinct seasons, making it unique.  
+Here are 5 facts ***about*** Bangladesh:
+
+[link](https:/google.com/)
+
+- List 1
+- List 2
+
+1. List 1
+2. List 2
+
+1. Nested List
+    - List 2
+
+```python
+
+print
+
+
+```
+
+| Name | Roll |
+|------|------|
+|CSE| 2100|
+
+- [x  ] Install Git
+- [ ]
+
+![ ] (./ )
