98833573ce4a1022a200db86d40ff82cd9f5c039
Layama09 <layamamaisha0409@gmail.com>
2026-06-02 22:31:40 +0600

    Add Contact class with getters and setters

--- MainPhonecontactUI.java
+++ MainPhonecontactUI.java
@@ -116,5 +116,53 @@ public class MainPhonecontactUI extends Application {
 // ===== Part 3 (srabone) =====
 // update/delete/search logic
 
-  
+Scene scene = new Scene(layout, 650, 500);
+
+        stage.setTitle("Phone Contact UI");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static class Contact {
+
+        private String name;
+        private String phone;
+        private String email;
+
+        public Contact(String name, String phone, String email) {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+  }
 
