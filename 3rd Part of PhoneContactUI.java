3f06898f9f83a06e3708368da8871254450d0e76
Layama09 <layamamaisha0409@gmail.com>
2026-06-02 22:30:38 +0600

    Update 3rd Part of PhoneContactUI.java

--- 3rd Part of PhoneContactUI.java
+++ 3rd Part of PhoneContactUI.java
@@ -1,12 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+Scene scene = new Scene(layout, 650, 500);
 
-/**
- *
- * @author HP
- */
-public class NewClass1 {
-    
-}
+        stage.setTitle("Phone Contact UI");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static class Contact {
+
+        private String name;
+        private String phone;
+        private String email;
+
+        public Contact(String name, String phone, String email) {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+  }
